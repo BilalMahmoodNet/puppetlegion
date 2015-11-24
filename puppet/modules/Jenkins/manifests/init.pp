d224fccbad8c2d79465d4533c170a93cca7eaf17
Joseph-Irving <joseph.irving500@gmail.com>
2015-11-24 11:09:40 +0000

    jenkins

---
+++ puppet/modules/Jenkins/manifests/init.pp
@@ -0,0 +1,36 @@
+class jenkins {
+  Exec {
+    path => [
+	  '/usr/local/bin',
+	  '/opt/local/bin',
+	  '/usr/bin',
+	  '/bin',
+	  '/sbin'],
+	logoutput => true,
+  }
+  
+  exec { 'jenkins_down':
+    cwd     => '/opt/',
+	command => 'wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -',
+	notify  => Exec['jenkins_write'],
+  }
+  
+  exec { 'jenkins_write':
+    command => "sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'",
+	notify  => Exec['jenkins_update'],
+	user    => root,
+  }
+  
+  exec { 'jenkins_update':
+    command => 'apt-get update',
+	user    => root,
+	notify  => Exec['jenkins_install'],
+  }
+  
+  exec { 'jenkins_update':
+    command => 'apt-get install jenkins',
+	user    => root,
+  }
+	
+
+}
